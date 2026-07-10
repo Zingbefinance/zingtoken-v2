9a7e7199b31a8fa7580d346b995645f34c7c28cf
GOGBE ZINGBE MARCELIN <zingbefinance123@gmail.com>
2026-07-10 11:10:16 +0200

    Replace Vercel URL with custom domain

--- app/app/robots.ts
+++ app/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://zingtoken-v2.vercel.app/sitemap.xml",
+    sitemap: "https://www.zingtoken.org/sitemap.xml",
   };
 }
--- app/app/sitemap.ts
+++ app/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://zingtoken-v2.vercel.app",
+      url: https://www.zingtoken.org"",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
