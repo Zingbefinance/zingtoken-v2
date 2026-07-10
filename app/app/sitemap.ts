920a4549bde0534472ddeda11eb2b3b7213152b6
GOGBE ZINGBE MARCELIN <zingbefinance123@gmail.com>
2026-07-10 13:20:18 +0200

    Add SEO metadata, robots and sitemap

--- app/app/sitemap.ts
+++ app/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: https://www.zingtoken.org"",
+      url: "https://www.zingtoken.org",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
